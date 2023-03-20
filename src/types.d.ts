5ebbfa0124c61a1738a5e9048a52515072e36926
kylym <kylym.mv@gmail.com>
2023-03-20 17:47:14 +0600

    add types

---
+++ src/types.d.ts
@@ -0,0 +1,6 @@
+import AnimeStarsParser from "./lib/animestars";
+import YummyAnimeParser from "./lib/yummyanime";
+
+
+export type YummyAnimeTypes = YummyAnimeParser;
+export type AnimeStarsTypes = AnimeStarsParser;
